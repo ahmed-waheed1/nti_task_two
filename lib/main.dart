5d3593aa23d5e48bcc3a9ff54f8f64bf6322d3ad
Ahmed Waheed <ahm761171@gmail.com>
2025-03-03 01:56:25 +0200

    Update main app entry point to set HomePage as the default home widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'features/Home/presentation/pages/home_page.dart';
-import 'features/Home/presentation/pages/home_page_empty.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +31,7 @@ class MyApp extends StatelessWidget {
       // },
       title: 'Todo App',
       // initialRoute: '/',
-      home: const HomePageEmpty(),
+      home: const HomePage(),
     );
   }
 }
